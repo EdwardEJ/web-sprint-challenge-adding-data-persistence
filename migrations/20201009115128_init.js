b11a0d73bb1cde0dc278e7cb1bf7856a7d0e43db
A <jesus.e.estrada@hotmail.com>
2020-10-09 12:36:21 -0500

    created table

--- migrations/20201009115128_init.js
+++ migrations/20201009115128_init.js
@@ -3,27 +3,57 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('projects', tbl => {
       tbl.increments();
-      tbl.string('name').notNullable()
-      tbl.text('description')
+      tbl.string('name').notNullable();
+      tbl.text('description');
       tbl.boolean('complete').defaultTo(false);
     })
+
     .createTable('tasks', tbl => {
-    tbl.increments();
-    tbl.
-    tbl.
+      tbl.increments();
+      tbl.string('description').notNullable();
+      tbl.text('notes');
+      tbl.boolean('completed').defaultTo(false);
+
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('restrict')
+        .onUpdate('cascade');
     })
-    .createTable('task_resources', tbl => {
-    tbl.increments();
-    tbl.
-    tbl.
-    })
+
     .createTable('resources', tbl => {
-    tbl.increments();
-    tbl.
-    tbl.
-  })
+      tbl.increments();
+      tbl.string('name').unique();
+      tbl.string('description');
+    })
+
+    .createTable('task_resources', tbl => {
+      tbl.increments();
+
+      tbl.
+        integer('tasks_id')
+        .unsigned()
+        .references('id')
+        .inTable('tasks')
+        .onDelete('restrict')
+        .onUpdate('cascade');
+      tbl.
+        integer('resources_id')
+        .unsigned()
+        .references('id')
+        .inTable('resources')
+        .onDelete('restrict')
+        .onUpdate('cascade');
+
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('task_resources');
 };
